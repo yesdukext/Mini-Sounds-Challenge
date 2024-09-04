08e9c6318c12dba422d8dc94b82b770ed42fa84c
yesdukext <yves.dukuze.ext@bbc.co.uk>
2024-09-04 11:32:20 +0100

    setting up

---
+++ MiniSoundDemo/MiniSoundDemo/MiniSoundDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniSoundDemoApp.swift
+//  MiniSoundDemo
+//
+//  Created by Yves Dukuze on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiniSoundDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
